676ec6bd788f210382a19bf79302b7fb3a94d070
Miley Wright <dishaliekwright@yahoo.com>
2020-02-19 23:48:55 -0500

    GET request for users

---
+++ users/users.router.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const Users = require('./users-model');
+// const restricted = require('../auth/restricted-middleware.js');
+
+router.get('/', (req,res) => {
+    User.find()
+        .then(user => {
+            res.status(200).json(users)
+        })
+        .catch(err => {
+            console.log(err)
+            res.send(err)
+        })
+})
+
+//GET auth/user/dept
+router.get('/dept', (req, res) => {
+    
+})
+
+module.exports = router;
