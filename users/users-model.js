0d3ffd62904fe1fe13f5639220f5e86c78980a56
Miley Wright <dishaliekwright@yahoo.com>
2020-02-19 23:27:21 -0500

    user models

--- users/users-model.js
+++ users/users-model.js
@@ -1,5 +1,35 @@
 const db = require('../database/dbConfig');
 
 module.exports = {
-    add
+    add,
+    find, 
+    findBy,
+    findById,
+    findDept
+}
+
+function add(user) {
+    return db('users')
+    .insert(user)
+}
+
+function find() {
+    return db('users')
+    .select('id', 'username', 'password', 'department')
+}
+
+function findBy(filter) {
+    return db('user')
+    .where(filter);
+}
+
+function findById(id) {
+    return db('users')
+    .where({id})
+}
+
+function findDept(department) {
+    return db('users')
+    .select('id', 'username', 'department')
+    .where({department})
 }
